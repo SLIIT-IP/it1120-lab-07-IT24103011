712bbc157450b38dc1a17b7665c20fd0cab65c34
IT24103011 <it24103011@my.sliit.lk>
2024-10-02 07:35:41 +0530

    Add files via upload

---
+++ Q2/IT24103011Lab7Q2B.java
@@ -0,0 +1,14 @@
+class IT24103011Lab7Q2B {
+    public static void main(String[] args) {
+        
+        for(int i=0;i<5;i++){
+             System.out.print((i+1)+" - ");
+            for(int j=0;j<(i+1);j++){
+            System.out.print("* ");
+         }
+          System.out.println("");
+        }
+        
+       
+    }
+}
