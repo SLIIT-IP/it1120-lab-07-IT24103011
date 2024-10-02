4f74a2cf6d4f80872aa68e06dd4109d8fbbb1f56
IT24103011 <it24103011@my.sliit.lk>
2024-10-02 07:28:56 +0530

    Add files via upload

---
+++ Q2/IT24103011Lab7Q2A.java
@@ -0,0 +1,13 @@
+class IT24103011Lab7Q2A {
+    public static void main(String[] args) {
+        
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+            System.out.print("$ ");
+         }
+          System.out.println("");
+        }
+        
+       
+    }
+}
